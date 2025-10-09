4f1a68a231ab40ae24b8acbd30d9e3e0dad415b1
Efran <efran.quispe13@gmail.com>
2025-10-09 06:37:59 -0500

    update frontend

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import Toast, { POSITION } from 'vue-toastification'; // Importa POSITION para configurar la ubicación
-import 'vue-toastification/dist/index.css'; // Importa los estilos base
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
 import App from './App.vue';
 import router from './router';
@@ -9,22 +9,8 @@ import './assets/css/main.css';
 
 const app = createApp(App);
 
-// Opciones personalizadas para las notificaciones
-const toastOptions = {
-    position: POSITION.TOP_RIGHT,
-    timeout: 5000, // 5 segundos
-    closeOnClick: true,
-    pauseOnFocusLoss: true,
-    pauseOnHover: true,
-    draggable: true,
-    // Aquí es donde asignamos nuestra clase CSS personalizada
-    toastClassName: {
-        success: "toast-success-brand"
-    }
-};
-
 app.use(createPinia());
 app.use(router);
-app.use(Toast, toastOptions); // Pasamos las opciones al registrar el plugin
+app.use(Toast); // No se necesitan opciones especiales para esto
 
 app.mount('#app');
